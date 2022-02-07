3d4db5e286f3abc8100330a9a45bc39d43aab7bd
shuhai <yangshuhai@pdnews.cn>
2022-02-07 10:10:34 +0800

    update

--- trunk/run.sh
+++ trunk/run.sh
@@ -1,6 +1,7 @@
-./configure --osx --ffmpeg-tool=on
+#./configure --osx --ffmpeg-tool=on
 make -j4
 
+ulimit -HSn 1107
 if [ "$?" == "0" ]; then
     ./etc/init.d/srs stop
     ./etc/init.d/srs start
